fc68d518cad37b4512b08caa3df2ec456f918e70
AWITI0301 <johnoketchawiti@gmail.com>
2022-06-08 01:10:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+customerName = 'bob'
+// customerName = "bob".toUpperCase()
+const leastFavoriteCustomer = "blue"
+
+
+function upperCaseCustomerName(){
+    // console.log(name +' '+ customerName.toUpperCase())
+    // cust = customerName.toUpperCase()
+    // console.log(cust)
+    customerName = customerName.toUpperCase()
+    
+}
+//upperCaseCustomerName()
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+  }
+  
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+  }
+  
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'red'
+  }
+// const myVar = "Foo";
+
+// function first() {
+//   console.log("Inside first()");
+
+//   console.log("myVar is currently equal to:", myVar);
+// }
+// function second(){
+//     const myVar = "Bar"
+//     first()
+// }
+// first()
+// second()
